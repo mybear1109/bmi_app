4f8185a6562b278c30867f7837f18368c6f846b8
mybear1109 <mybear8211@gmail.com>
2025-02-22 05:55:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # ✅ 페이지 설정
-st.set_page_config(page_title="건강 관리 앱", page_icon="🏥", layout="wide")
+st.set_page_config(page_title=" AI 피트니스 코치", page_icon="🏠", layout="wide")
 import json
 import os
 from sidebar import get_selected_menu
